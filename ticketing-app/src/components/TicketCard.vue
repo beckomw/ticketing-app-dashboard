c8c06acd0bd813fc2982b511c22ee44014b29146
beckomw <Waynebeckom@gmail.com>
2025-04-12 23:56:31 -0600

    vue updates

---
+++ ticketing-app/src/components/TicketCard.vue
@@ -0,0 +1,25 @@
+<template> 
+  <div class="card mb-3"> 
+    <div class="card-body"> 
+      <h5 class="card-title"> {{ title }} </h5>  
+      <p class="card-text"> {{ description }} </p> 
+      <button class="btn btn-primary"> View Details </button>
+    </div> 
+  </div> 
+</template> 
+
+
+
+<script> 
+
+  export default {
+    props: {
+      
+    }
+  
+
+
+
+
+
+<script> 
